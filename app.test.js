737fb3f71a70f94c78de9f479ef39cfc149f6b1c
kasicons479 <kasivishwanathan.thirumalaisankar@chainsys.com>
2026-09-02 17:51:22 +0530

    added subtract

--- app.test.js
+++ app.test.js
@@ -3,3 +3,9 @@ const { add } = require("./app");
 test("adds two numbers", () => {
     expect(add(2, 3)).toBe(5);
 });
+
+const { subtract } = require("./app");
+
+test("subtract 2 numbers", () => {
+    expect(subtract(5, 3)).toBe(2);
+})
